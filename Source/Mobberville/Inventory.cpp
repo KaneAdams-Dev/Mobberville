233c1ec2797da9c52daa4f4dfff4a07b3cc5c6a0
varkxel <89874295+varkxel@users.noreply.github.com>
2023-07-04 13:26:02 +0100

    Inventory system getter functions + Test

--- Source/Mobberville/Inventory.cpp
+++ Source/Mobberville/Inventory.cpp
@@ -20,12 +20,16 @@ void UInventory::AddItem(AItem* item, int64 count)
 	
 	if (!items.Contains(itemID))
 	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, TEXT("Created item"));
+		
 		// Item does not exist, create the entries.
 		items.Add(itemID, item);
 		itemCounts.Add(itemID, count);
 	}
 	else
 	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, TEXT("Added to item"));
+
 		// Item exists, add the count to it.
 		itemCounts[itemID] += count;
 	}
@@ -57,4 +61,30 @@ void UInventory::RemoveItem(AItem* item, int64 count)
 		itemCounts.Remove(itemID);
 		items.Remove(itemID);
 	}
+}
+
+bool UInventory::ItemExists(const FString& id)
+{
+	return items.Contains(id) && itemCounts.Contains(id);
+}
+
+AItem* UInventory::GetItem(const FString& id)
+{
+	if (!ItemExists(id))
+	{
+		// Return nullptr if there is no item.
+		// TODO make safer way of handling this.
+		return nullptr;
+	}
+	return items[id];
+}
+
+int64 UInventory::GetItemCount(const FString& id)
+{
+	if (!ItemExists(id))
+	{
+		// Item doesn't exist, so there is 0 of the item.
+		return 0;
+	}
+	return itemCounts[id];
 }
--- Source/Mobberville/Inventory.h
+++ Source/Mobberville/Inventory.h
@@ -20,6 +20,15 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Inventory")
 	void RemoveItem(AItem* item, int64 count = 1);
 
+	UFUNCTION(BlueprintCallable, Category = "Inventory")
+	bool ItemExists(const FString& id);
+
+	UFUNCTION(BlueprintCallable, Category = "Inventory")
+	AItem* GetItem(const FString& id);
+
+	UFUNCTION(BlueprintCallable, Category = "Inventory")
+	int64 GetItemCount(const FString& id);
+
 protected:
 	UPROPERTY(BlueprintReadWrite, EditAnywhere)
 	TMap<FString, AItem*> items;
